43ed114534035ecb5747b08c1e81fccf0d867dda
Luis Ignacio Viladevall de Lecea <60795165+luisVDL@users.noreply.github.com>
2023-02-02 18:23:42 +0100

    click sound

--- Mobile game/main/Conejo.go
+++ Mobile game/main/Conejo.go
@@ -81,3 +81,26 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "Rabbit_Click"
+  type: "sound"
+  data: "sound: \"/assets/Sounds/boing-spring-mouth-harp-04-20-13-4-103346.wav\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
